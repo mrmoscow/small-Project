6c94a1a1a32888720caf79e4bc721f7304aa67ea
ShengFeng Yen <mrmoscow@gmail.com>
2023-07-02 12:00:46 +0800

    clean

---
+++ classNote/test-2.py
@@ -0,0 +1,30 @@
+
+a='Open-AI-class2,..This'
+
+print(a)
+print(a[0])
+print(a[1])
+print(a[10])
+
+print(a[-1])
+print(a[-3])
+
+print(type(3))
+print(type(3.14))
+#print(type{1:3.14,2:6.28})
+
+print('Test','\a')
+
+al=['Hello','World','first']
+
+print(al[0],al[2])
+#print(al[0],)
+
+c="Can  you tell me the story about the Harry Portter"
+myTuple = ("John", "Peter", "Vicky")
+x1 = "".join(myTuple)
+x2 ="".join(al)
+
+print(c.split())
+print(x1)
+print(x2)
